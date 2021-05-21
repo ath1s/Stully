8c750986f3d366dd367ddf222c904238acf23075
Mitchell van der Klij <mitchell.vanderklij@ictmbo.nl>
2021-05-21 11:56:21 +0200

    added login & registration

--- pages/registration.php
+++ pages/registration.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stully - Login</title>
+    <title>Stully - Registratie</title>
 </head>
 <body>
-    <form method="POST" action="php/verify_login.php">
-        <h1>Login</h1>
+    <form method="POST" action="php/verify_registration.php">
+        <h1>Registratie</h1>
         <input type="text" placeholder="Gebruikersnaam" name="username">
-        <input type="password" placeholder="Wachtwoord" name="password">
+        <input type="password" placeholder="Wachtwoord" name="password1">
+        <input type="password" placeholder="Herhaling wachtwoord" name="password2">
+        <input type="email" placeholder="E-mail" name="email">
         <input type="submit" value="Login">
         <?php
-            echo "<h2 style='color:green;'>" . $_GET["accountmade"] . "</h2>";
             echo "<h2 style='color:red;'>" . $_GET["error"] . "</h2>";
         ?>
     </form>
