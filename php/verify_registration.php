4af25d5f1770a865024b150617d0c8ebba82c66f
Mitchell van der Klij <mitchell.vanderklij@ictmbo.nl>
2021-05-24 21:05:55 +0200

    added styling in dev branch, sorry Nielsgit add -A

--- php/verify_registration.php
+++ php/verify_registration.php
@@ -6,6 +6,6 @@
     if ($verification->register($_POST["username"], $_POST["password1"], $_POST["password2"], $_POST["email"])) {
         header("Location: ../index.php?accountmade=Je%20account%20is%20aangemaakt.%20Je%20kan%20nu%20inloggen.");
     } else {
-        header("Location: ../pages/registration.php?error=Dit%20zijn%20de%20verkeerde%20gegevens.");
+        header("Location: ../pages/registration.php?error=Er%20is%20een%20fout%20opgetreden.");
     }
 ?>
