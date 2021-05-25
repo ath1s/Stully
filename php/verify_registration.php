d8079b4155fa0125cc3415a6bc3f311598d45f42
Mitchell van der Klij <mitchell.vanderklij@ictmbo.nl>
2021-05-25 11:37:06 +0200

    fixes again

--- php/verify_registration.php
+++ php/verify_registration.php
@@ -3,7 +3,7 @@
     require_once("../classes/Account.php");
 
     $verification = new Account();
-    if(!empty($_POST["username"]) || !empty($_POST["password1"]) || !empty($_POST["password2"]) || !empty($_POST["email"])){
+    if(!empty($_POST["username"]) && !empty($_POST["password1"]) && !empty($_POST["password2"]) && !empty($_POST["email"])){
         if ($verification->register($_POST["username"], $_POST["password1"], $_POST["password2"], $_POST["email"])) {
             header("Location: ../index.php?accountmade=Je%20account%20is%20aangemaakt.%20Je%20kan%20nu%20inloggen.");
         } else {
