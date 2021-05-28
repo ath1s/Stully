d7d4784ae806741addb75f9144e99c6f9a633179
Mitchell van der Klij <mitchell.vanderklij@ictmbo.nl>
2021-05-28 15:19:32 +0200

    fixed typo

--- php/redirect2.php
+++ php/redirect2.php
@@ -5,7 +5,7 @@ $status = new Account;
 if($_SESSION["loggedin"] == true){
     if($status->getStatus($_SESSION["username"]) == "oncall" || $status->getStatus($_SESSION["username"]) == "available"){
         $status->changeStatus($_SESSION["username"], "online");
-        $status->changeStatus($_SESSION["username2"], "availble");
+        $status->changeStatus($_SESSION["username2"], "available");
         header("location:../pages/status.php");
     }else{
         header("Location:../pages/status.php");
