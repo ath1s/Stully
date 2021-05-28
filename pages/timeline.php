ad5fdefca348bdb644a679e2dbe85484b7b4b9f5
Mitchell van der Klij <mitchell.vanderklij@ictmbo.nl>
2021-05-28 22:09:02 +0200

    fixed jquery error

--- pages/timeline.php
+++ pages/timeline.php
@@ -13,8 +13,7 @@ if($_SESSION["loggedin"] != true){
     <title>Stully - Timeline</title>
     <script
         src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-        crossorigin="anonymous">
+        >
     </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
@@ -121,11 +120,11 @@ if($_SESSION["loggedin"] != true){
                                 echo '<div class="card mb-3 shadow-sm" style="background: white; opacity: none;">';
                                 echo '<div class="card-body">';
 
-                                echo '<h4 class="card-title">' . $timeline->getPosts()[$i]["title"] . '</h4>';
-                                echo '<p class="card-text">' . $timeline->getPosts()[$i]["code"] . '</p>';
-                                echo '<p class="card-text">' . $timeline->getPosts()[$i]["subtext"] . '</p>';
-                                echo '<p class="card-text">' . $timeline->getPosts()[$i]["timestamp"] . '</p>';
-                                echo '<a href="post.php?id=' . $timeline->getPosts()[$i]["post_id"] . '" class="btn btn-outline-primary">Reageer</a>';
+                                echo '<h4 class="card-title">' . $item["title"] . '</h4>';
+                                echo '<p class="card-text">' . $item["code"] . '</p>';
+                                echo '<p class="card-text">' . $item["subtext"] . '</p>';
+                                echo '<p class="card-text">' . $item["timestamp"] . '</p>';
+                                echo '<a href="post.php?id=' . $item["post_id"] . '" class="btn btn-outline-primary">Reageer</a>';
 
                                 echo '</div>';
                                 echo '</div>';
