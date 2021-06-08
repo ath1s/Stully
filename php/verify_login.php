d8b8903eb7184e237c05a9ce7ce0f1e48510e0b6
levi olde scheper <levi.oldescheper@gmail.com>
2021-06-08 11:22:47 +0200

    version 0.3

---
+++ php/verify_login.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    require_once("../classes/Account.php");
+
+    $verification = new Account();
+    if (!empty($_POST["username"]) || !empty($_POST["password"])) {
+        if ($verification->inloggen($_POST["username"], $_POST["password"])) {
+            $_SESSION["loggedin"] = true;
+            $_SESSION["username"] = $_POST["username"];
+            $status_change = new Account();
+            $status_change->changeStatus($_SESSION["username"], "online");
+            header("Location: ../pages/timeline.php");
+        } else {
+            header("Location: ../index.php?error=Dit%20zijn%20de%20verkeerde%20gegevens.");
+        }
+    } else {
+        header("Location: ../index.php?error=Dit%20zijn%20de%20verkeerde%20gegevens.");
+    }
+?>
